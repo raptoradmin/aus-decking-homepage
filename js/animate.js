ec9dcec1e475b2bd0561642ba3d770b0fdec36f9
Adam Miller <adam@raptorit.net>
2020-11-05 15:05:04 -0800

    Upgraded carousel, made navbar light

--- js/animate.js
+++ js/animate.js
@@ -12,15 +12,38 @@ $(window).scroll(function(){
 	scrollspy()
 })
 
-var anime_service = anime({
-	targets: '#sect-service',
-	translateY: ['40vh', 0],
-	delay: 800,
+var anime_nav = anime({
+	targets: '#sect-nav',
+	translateY: ['-100%', 0],
+	delay: 1000,
+	duration: 600,
+	easing: 'easeOutCubic'
+})
+
+var anime_overlay = anime({
+	targets: '.carousel-overlay *',
+	translateX: ['-100vw', 0],
+	delay: anime.stagger(120, {start: 800}),
 	easing: 'spring(1, 80, 12, 1)'
 })
 
+// var anime_service = anime({
+// 	targets: '#sect-service',
+// 	translateY: ['40vh', 0],
+// 	easing: 'spring(1, 80, 12, 1)',
+// 	autoplay: false
+// })
+
 var anime_partners = anime({
 	targets: '#sect-partners .fade-up',
+	top: 0,
+	opacity: 1,
+	delay: function(el) { return el.offsetLeft/2 },
+	autoplay: false
+})
+
+var anime_products = anime({
+	targets: '#sect-products .fade-up',
 	top: 0,
 	opacity: 1,
 	delay: function(el) { return el.offsetLeft/2 },
@@ -39,14 +62,14 @@ var anime_smbg = anime({
 })
 
 var elems = [{
+	tgt: '#sect-products .fade-up',
+	anime: anime_products
+},{
 	tgt: '#sect-partners .fade-up',
 	anime: anime_partners
 },{
 	tgt: '#sect-smbg .fade-sd',
 	anime: anime_smbg
-},{
-	tgt: '#sect-service',
-	anime: anime_service
 }]
 
 function scrollspy() {
