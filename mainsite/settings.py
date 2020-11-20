b181072f79d56ebdfde0c7ba5c1a0d311d1c1a96
Adam Miller <adam@raptorit.net>
2020-11-19 18:27:38 -0800

    Revert "Updated STATIC_ROOT"
    This reverts commit a75d8b2485b046239ea91a49f867933d94912378.

--- mainsite/settings.py
+++ mainsite/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/collectstatic/'
+STATIC_ROOT = '/static/'
 STATICFILES_DIRS = (
 	BASE_DIR / 'static',
 	BASE_DIR / 'products',
